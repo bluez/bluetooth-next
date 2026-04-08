0d0d81da09141abf4a44983fc32585c6a0289c46
kaihsin <kaihsin.chung@synaptics.corp-partner.google.com>
2026-04-08 16:32:17 +0800

    Bluetooth: Add Synaptcis 4384 Chip Support
    Signed-off-by: kaihsin <kaihsin.chung@synaptics.com>

--- drivers/bluetooth/hci_bcm.c
+++ drivers/bluetooth/hci_bcm.c
@@ -1594,6 +1594,7 @@ static const struct of_device_id bcm_bluetooth_of_match[] = {
 	{ .compatible = "brcm,bcm4335a0" },
 	{ .compatible = "cypress,cyw4373a0-bt", .data = &cyw4373a0_device_data },
 	{ .compatible = "infineon,cyw55572-bt", .data = &cyw55572_device_data },
+	{ .compatible = "brcm,bcm4384-bt" },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, bcm_bluetooth_of_match);
