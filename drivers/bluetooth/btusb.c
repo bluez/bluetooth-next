d73a0234babc85832b5330db8af8c549531d6e50
Zijun Hu <zijun.hu@oss.qualcomm.com>
2026-06-25 22:19:57 -0700

    Bluetooth: btusb: Use & instead of == to test bitflag BTUSB_IGNORE
    The driver_info field is a bitmask, so use & instead of == to test the
    BTUSB_IGNORE bitflag against it, which is consistent with how the other
    flags are tested.
    
    Reviewed-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Signed-off-by: Zijun Hu <zijun.hu@oss.qualcomm.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -4111,7 +4111,7 @@ static int btusb_probe(struct usb_interface *intf,
 			id = match;
 	}
 
-	if (id->driver_info == BTUSB_IGNORE)
+	if (id->driver_info & BTUSB_IGNORE)
 		return -ENODEV;
 
 	if (id->driver_info & BTUSB_ATH3012) {
